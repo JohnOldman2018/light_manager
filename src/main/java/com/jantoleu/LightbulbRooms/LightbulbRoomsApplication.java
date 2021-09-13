73c9d6975d0f5875ce5bb7358fa961e309a844ea
JohnOldman2018 <zhanbaur@gmail.com>
2021-09-13 20:03:37 +0600

    first commit

---
+++ src/main/java/com/jantoleu/LightbulbRooms/LightbulbRoomsApplication.java
@@ -0,0 +1,13 @@
+package com.jantoleu.LightbulbRooms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LightbulbRoomsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LightbulbRoomsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jantoleu/LightbulbRooms/LightbulbRoomsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jantoleu.LightbulbRooms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LightbulbRoomsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
